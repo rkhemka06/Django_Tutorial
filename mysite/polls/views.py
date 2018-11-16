47ae3889afec32d0e52a5efca457461f6b591003
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-16 12:03:24 -0800

    feet(tutorial1)
    completed tutorial 1 from django
    
    ITRAINING-202

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, ATRI. You are at polls index.")
+    return HttpResponse("Hello, ATRI. You are at polls ")
