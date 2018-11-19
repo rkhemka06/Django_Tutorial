663b66e5481e52715091b3223e3f9999048db187
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-19 11:41:23 -0800

    feat(Django Tutorial):part 3 completed
    learned about views in django
    
    ICTRAINING-204

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import HttpResponse
-
+from django.shortcuts import render,HttpResponse,get_object_or_404
+from django.http import Http404
 # Create your views here.
+from django.template import loader
 
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, ATRI. You are at polls ")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
