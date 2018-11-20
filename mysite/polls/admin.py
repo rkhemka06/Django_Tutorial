05e8b50fad6b9719a077694c5028f7e9dcae3c68
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-19 17:52:26 -0800

    feat(Django Tutorial): part6 and part 7 of tutorial
    Customized the application by using using stylesheet and updating the view for admin to add choices for a questions
    
    ICTRAINING-207 and ICTRAINING-208

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
