af142af8d81397cfe183a58481f2248925141fdc
RAJAT KHEMKA <rkhemka@atrihub.io>
2018-11-19 16:20:11 -0800

    feat(Django Tutorial): part5 of tutorial
    learned how to write test in django and importance of testing in project development
    
    ICTRAINING-206

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
+from django.utils import timezone
 
 
 def index(request, question_id):
@@ -36,12 +37,20 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
+#Question.objects.filter(pub_date__lte=timezone.now()) returns a queryset containing Questions whose pub_date is less
+# than or equal to - that is, earlier than or equal to - timezone.now.
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
